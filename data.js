96ed75955e6dbf11dfe94988b40f04877be93728
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2019-02-19 14:13:51 +0100

    Added french version

--- data.js
+++ data.js
@@ -1,114 +1,229 @@
 var data = {
-    "me": {
-        "last_name": "DUTRIEUX",
-        "first_name": "Cyril",
-        "avatar": "avatar.jpg",
-        "title": "Software engineer",
-        "description_short" : ["Various technologies engineer, specialized in embedded software, software security for mobile payment and IoT."],
-        "description": [
-            "I'm a various technologies engineer, specialized in embedded software.",
-            "I'm passionate, curious, and with an open (source) mind.",
-            "I have developed a wide range of skills in hardware digital design all the way to web stuffs from my professional and personal experiences.",
-            "31 years old, I am married, with a child.",
-            "Finally I'm an intensive sportsman, essentially cyclism, that's big part of my free time",
-        ],
-        "contact" : {
-            "location": "Living in Lyon",
-            "phone": "+336 81 21 92 48",
-            "email": "cyril.dutrieux@gmail.com",
-            "linkedin": "https://fr.linkedin.com/in/cyrildutrieux",
-            "github": "https://github.com/cdtx"
-        }
-    },
-    "experiences": [
-        {
-            "title": "Smart Card secured embedded software (2012-2016)",
+    "en": {
+        "me": {
+            "last_name": "DUTRIEUX",
+            "first_name": "Cyril",
+            "avatar": "avatar.jpg",
+            "title": "Software engineer",
+            "description_short" : ["Various technologies engineer, specialized in embedded software, software security for mobile payment and IoT."],
             "description": [
-                "Firmware developer in the team in charge of Secure Element technologies.",
-                "    - GlobalPlatform for cards specifications technical referent",
-                "    - Involved in architecture definition",
-                "    - Source code workflow and compilation-chain definition",
+                "I'm a various technologies engineer, specialized in embedded software.",
+                "I'm passionate, curious, and with an open (source) mind.",
+                "I have developed a wide range of skills in hardware digital design all the way to web stuffs from my professional and personal experiences.",
+                "34 years old, I am married, with 2 children.",
+                "Finally I'm an intensive sportsman, essentially cyclism, that's big part of my free time",
             ],
-            "keywords": [
-                "GlobalPlatform, Java Card, NFC, ISO 7816, C, Arm, Avr, Cryptography basics, Subversion, SCons",
-            ],
-            "distinctions" : [
-                "VaultSecure product received EMVCo security evaluation approval"
-            ],
-            "company": {
-                "name": "Inside Secure",
-                "logo": "insidesecure.png"
+            "contact" : {
+                "location": "Living in Lyon",
+                "phone": "+336 81 21 92 48",
+                "email": "cyril.dutrieux@gmail.com",
+                "linkedin": "https://fr.linkedin.com/in/cyrildutrieux",
+                "github": "https://github.com/cdtx"
             }
         },
-        {
-            "title": "Python expert (2012-2016)",
-            "description": [
-                "Aware of productivity and simplicity",
-                "Created and maintained:",
-                "    - Full APDU framework for smart Cards communications",
-                "    - Full python test engine",
-                "    - Driven trainee on \"Using python to execute GlobalPlatform compliance test plans\"",
-                "    - Managed the voluntary contributions.",
-            ],
-            "keywords": [
-                "Python, PyScard, integration & management",
-            ],
-            "company": {
-                "name": "Inside Secure",
-                "logo": "insidesecure.png"
-            }
-        },
-        {
-            "title": "Embedded software (2008-2012)",
-            "description": [
-                "Developer on a new generation of desk driver display",
-            ],
-            "keywords" : [
-                "C, QNX, photon, ClearCase"
-            ],
-            "distinctions": [
-                "Worldwide internal award of the most innovative project of the year 2015"
-            ],
-            "company": {
-                "name": "Alstom transport",
-                "logo": "alstom.jpg"
+        "experiences": [
+            {
+                "title": "Smart Card secured embedded software (2012-2016)",
+                "description": [
+                    "Firmware developer in the team in charge of Secure Element technologies.",
+                    "    - GlobalPlatform for cards specifications technical referent",
+                    "    - Involved in architecture definition",
+                    "    - Source code workflow and compilation-chain definition",
+                ],
+                "keywords": [
+                    "GlobalPlatform, Java Card, NFC, ISO 7816, C, Arm, Avr, Cryptography basics, Subversion, SCons",
+                ],
+                "distinctions" : [
+                    "VaultSecure product received EMVCo security evaluation approval"
+                ],
+                "company": {
+                    "name": "Inside Secure",
+                    "logo": "insidesecure.png"
+                }
             },
-        },
-        {
-            "title": "Side projects",
-            "description" : [
-                "- Microchip PIC various experiences (sensors, standard buses, etc.)",
-                "- Embedded Linux with buildroot",
-                "- Personal and professional web sites",
-                "- Personal custom markdown language framework (<a href=\"https://github.com/cdtx/mino\">github.com/cdtx/mino</a>)",
-            ],
-        }
-    ],
-    "education": [
-        {
-            "title": "MSc in software engineering specialized in embedded software",
-            "description": [
-                "Both engineer and research degrees with:",
-                "    - Real time operating systems",
-                "    - Digital design (FPGA, Verilog)",
-                "    - Strong various electronic knowledges"
-            ],
-            "company": {
-                "name": "ENSEA",
-                "logo": "ensea.png"
+            {
+                "title": "Python expert (2012-2016)",
+                "description": [
+                    "Aware of productivity and simplicity",
+                    "Created and maintained:",
+                    "    - Full APDU framework for smart Cards communications",
+                    "    - Full python test engine",
+                    "    - Driven trainee on \"Using python to execute GlobalPlatform compliance test plans\"",
+                    "    - Managed the voluntary contributions.",
+                ],
+                "keywords": [
+                    "Python, PyScard, integration & management",
+                ],
+                "company": {
+                    "name": "Inside Secure",
+                    "logo": "insidesecure.png"
+                }
+            },
+            {
+                "title": "Embedded software (2008-2012)",
+                "description": [
+                    "Developer on a new generation of desk driver display",
+                ],
+                "keywords" : [
+                    "C, QNX, photon, ClearCase"
+                ],
+                "distinctions": [
+                    "Worldwide internal award of the most innovative project of the year 2015"
+                ],
+                "company": {
+                    "name": "Alstom transport",
+                    "logo": "alstom.jpg"
+                },
+            },
+            {
+                "title": "Side projects",
+                "description" : [
+                    "- Microchip PIC various experiences (sensors, standard buses, etc.)",
+                    "- Embedded Linux with buildroot",
+                    "- Personal and professional web sites",
+                    "- Personal custom markdown language framework (<a href=\"https://github.com/cdtx/mino\">github.com/cdtx/mino</a>)",
+                ],
             }
-        }
-    ],
-    "languages" : [
-        {
-            "name": "French",
-            "short": "fr",
-            "eval": 5
+        ],
+        "education": [
+            {
+                "title": "MSc in software engineering specialized in embedded software",
+                "description": [
+                    "Both engineer and research degrees with:",
+                    "    - Real time operating systems",
+                    "    - Digital design (FPGA, Verilog)",
+                    "    - Strong various electronic knowledges"
+                ],
+                "company": {
+                    "name": "ENSEA",
+                    "logo": "ensea.png"
+                }
+            }
+        ],
+        "languages" : [
+            {
+                "name": "French",
+                "short": "fr",
+                "eval": 5
+            },
+            {
+                "name": "English",
+                "short": "gb",
+                "eval": 3
+            }
+        ]
+    },
+    "fr": {
+        "me": {
+            "last_name": "DUTRIEUX",
+            "first_name": "Cyril",
+            "avatar": "avatar.jpg",
+            "title": "Ingénieur en informatique",
+            "description_short" : ["Ingénieur senior en développement informatique, spécialisé en logiciel embarqué, sécurité numérique, Python et développement web"],
+            "description": [
+                "Je suis ingénieur senior, spécialisé dans le logiciel embarqué.",
+                "Passionné, curieux et ouvert d'esprit.",
+                "En 10 ans j'ai cultivé des compétences allant du monde de l'électronique à celui du web.",
+                "34 ans, marié, 2 enfants.",
+                "Je suis passionné de cyclisme, que je pratique en compétition.",
+            ],
+            "contact" : {
+                "location": "Saint-Vulbas (01)",
+                "phone": "+336 81 21 92 48",
+                "email": "cyril.dutrieux@gmail.com",
+                "linkedin": "https://fr.linkedin.com/in/cyrildutrieux",
+                "github": "https://github.com/cdtx"
+            }
         },
-        {
-            "name": "English",
-            "short": "gb",
-            "eval": 3
-        }
-    ]
+        "experiences": [
+            {
+                "title": "Création de startup - PORTEYE (depuis 2016)",
+                "description": [
+                    "Service web d'expertise de voitures d'occasion",
+                    "Co-inventeur du concept",
+                    "Responsable du développement de la plateforme",
+                ],
+                "keywords": [
+                    "Python, Django, HTML, Sass, Javascript",
+                ],
+                "company": {
+                    "logo": "porteye.png"
+                }
+            },
+            {
+                "title": "Développeur indépendant (depuis 2016)",
+                "description" : [
+                    "Collecte et analyse de données eBay pour un client commerçant",
+                ],
+            },
+            {
+                "title": "Firmware sécurisé pour carte à puce (2012-2016)",
+                "description": [
+                    "Développeur de logiciel embarqué & Expert python",
+                    "    - Réferent sur les spécifications GlobalPlatform",
+                    "    - Impliqué dans les définitions d'architecture",
+                    "    - Création d'un framework de communication et de test pour cartes à puces",
+                ],
+                "keywords": [
+                    "C, ARM, AVR, GlobalPlatform, Java Card, NFC, ISO 7816, Cryptography basics, Python, PyScard",
+                ],
+                "distinctions" : [
+                    "Produit VaultSecure certifié conforme par EMVCo"
+                ],
+                "company": {
+                    "name": "Inside Secure",
+                    "logo": "insidesecure.png"
+                }
+            },
+            {
+                "title": "Ingénieur logiciel (2008-2012)",
+                "description": [
+                    "Développement d'une nouvelle génération de pupitres conducteur",
+                    "Déploiement d'un prototype d'outil de maintenance à distance d'une flotte de trains",
+                ],
+                "keywords" : [
+                    "C, QNX, photon, ClearCase"
+                ],
+                "company": {
+                    "name": "Alstom transport",
+                    "logo": "alstom.jpg"
+                },
+            },
+        ],
+        "education": [
+            {
+                "title": "Ingénieur en électronique et systèmes embarqués (2008)",
+                "description": [
+                    "Double diplome en parallèle d'un master recherche",
+                    "    - Systèmes d'exploitation temps réel",
+                    "    - Systèmes numériques (FPGA, Verilog)",
+                    "    - Electronique en tout genre"
+                ],
+                "company": {
+                    "name": "ENSEA",
+                    "logo": "ensea.png"
+                }
+            },
+            {
+                "title" : "BTS en mécanique et automatismes industriels",
+                "description" : [],
+                "company" : {
+                    "name": "",
+                    "logo": ""
+                }
+            },
+        ],
+        "languages" : [
+            {
+                "name": "Français",
+                "short": "fr",
+                "eval": 5
+            },
+            {
+                "name": "Anglais",
+                "short": "gb",
+                "eval": 3
+            }
+        ]
+    },
 }
